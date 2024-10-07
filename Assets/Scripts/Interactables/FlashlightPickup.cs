7290b40b9cd08a52594487c7cc92e4de248d2f1f
Retro <leonardobaldicera@gmail.com>
2024-10-07 10:17:51 -0600

    overall fixing

--- Assets/Scripts/Interactables/FlashlightPickup.cs
+++ Assets/Scripts/Interactables/FlashlightPickup.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FlashlightPickup : MonoBehaviour, IInteractable, ICollectable
 {
     public GameEvent Event;
+
+    public UnityEvent OnCollect;
 
     public void Collect()
     {
         AudioManagerFMOD.Instance.PlayOneShot(AudioManagerFMOD.Instance.SFXEvents.PickUpFlashlight, transform.position);
 
         gameObject.SetActive(false);//Destroy(this.gameObject);// uhhhhhhh disable it? pool this shit 
+
+        OnCollect.Invoke();
     }
 
     public void OnInteract()
