6464b881309c8c01ce7dd138b5c88dae9b4bc814
Musse <musse.hidru@outlook.com>
2024-10-30 02:01:19 -0600

    Range on change

--- Assets/Scripts/Flashlight/Ability/FlashlightAbility.cs
+++ Assets/Scripts/Flashlight/Ability/FlashlightAbility.cs
@@ -9,19 +9,23 @@ public abstract class FlashlightAbility : MonoBehaviour
 
     // Ability light settings
     [field: Header("Ability Light Settings")]
-    [field: SerializeField] public float AbilityIntensity { get; protected set; }
+    [field: SerializeField]
+    public float AbilityIntensity { get; protected set; }
+
     [field: SerializeField] public Color AbilityColor { get; protected set; }
     [field: SerializeField] public float AbilitySpotAngle { get; protected set; }
     [field: SerializeField] public float AbilityInnerSpotAngle { get; protected set; }
-    
+
     // Ability flashlight settings
     [field: Header("Flashlight Settings")]
-    [field: SerializeField] public float BaseIntensity { get; protected set; } = 25;
-    [field: SerializeField] public Color BaseColor { get; protected set; } 
+    [field: SerializeField]
+    public float BaseIntensity { get; protected set; } = 25;
+
+    [field: SerializeField] public Color BaseColor { get; protected set; }
     [field: SerializeField] public float BaseSpotAngle { get; protected set; } = 60;
     [field: SerializeField] public float BaseInnerSpotAngle { get; protected set; } = 30;
-    [field:SerializeField] public float BaseRange { get; protected set; } = 10;
-    
+    [field: SerializeField] public float BaseRange { get; protected set; } = 10;
+
     protected FlashLight Flashlight;
 
     public abstract void OnUseAbility();
@@ -29,10 +33,11 @@ public abstract class FlashlightAbility : MonoBehaviour
 
     public void SetLight(Light currentLight)
     {
-        currentLight.intensity = AbilityIntensity;
-        currentLight.color = AbilityColor;
-        currentLight.spotAngle = AbilitySpotAngle;
-        currentLight.innerSpotAngle = AbilityInnerSpotAngle;
+        currentLight.intensity = BaseIntensity;
+        currentLight.color = BaseColor;
+        currentLight.spotAngle = BaseSpotAngle;
+        currentLight.innerSpotAngle = BaseInnerSpotAngle;
+        currentLight.range = BaseRange;
     }
 
     public virtual void Initialize(FlashLight flashlight)
