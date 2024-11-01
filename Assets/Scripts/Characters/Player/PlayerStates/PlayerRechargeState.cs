8dc812d6a93a1beb1d47f75156786f694ccc3ed3
Musse <musse.hidru@outlook.com>
2024-10-31 22:11:40 -0600

    Reload WIP

--- Assets/Scripts/Characters/Player/PlayerStates/PlayerRechargeState.cs
+++ Assets/Scripts/Characters/Player/PlayerStates/PlayerRechargeState.cs
@@ -7,26 +7,31 @@ using Utilities;
 public class PlayerRechargeState : PlayerBaseState
 {
     public PlayerRechargeState
-        (PlayerController playerController) : base(playerController) { }
-    
-    private CountdownTimer timer;
-    private string reloadText = "Recharging";
-    
+        (PlayerController playerController) : base(playerController)
+    {
+    }
+
+    private float progress; // Reference to the progress bar UI.
+    private const float BaseRechargeRate = 0.5f; // Normal recharge rate per second.
+    private const float ButtonMashBoost = 0.5f; // Extra progress per button press.
+    private float maxTime; // Max value for the progress bar.
+
+
     public override void EnterState()
     {
+        progress = 0;
+        maxTime = Player.flashlight.MaxBatteryLife;
+     
         // playerAnimator.animator.Play(playerAnimator.DieHash);
         Player.ReloadAnimation = Player.StartCoroutine(ReloadAnimation());
         
-        timer = new CountdownTimer(Player.Settings.FlashlightReloadTime);
-        timer.Start();
-
         Player.playerFootsteps.getPlaybackState(out var playbackState);
         if (playbackState.Equals(PLAYBACK_STATE.PLAYING))
         {
             Player.playerFootsteps.stop(STOP_MODE.ALLOWFADEOUT);
         }
-
     }
+
     public override void ExitState()
     {
         Player.StopCoroutine(Player.ReloadAnimation);
@@ -36,57 +41,44 @@ public class PlayerRechargeState : PlayerBaseState
 
     public override void StateFixedUpdate()
     {
-      
     }
 
     public override void StateUpdate()
     {
-        timer.Tick(Time.deltaTime);
-        if (timer.IsFinished)
+        if(progress >= maxTime)
         {
-            //animation stuff here
             Player.ChangeState(Player.MoveState);
         }
-
-       
+        
+        progress += BaseRechargeRate * Time.deltaTime;
     }
     
+
     private IEnumerator ReloadAnimation()
     {
         while (true)
         {
+            Player.Event.SetTutorialText?.Invoke("Recharging: " + progress.ToString("F0"));
             yield return new WaitForSeconds(0.1f);
-            Player.Event.SetTutorialText?.Invoke(reloadText = reloadText == "Recharging . . ." ? "Recharging" : reloadText + " .");
         }
     }
 
-    public override void HandleMovement(Vector2 dir)
-    {
+    public override void HandleMovement(Vector2 dir) { }
 
-    }
-    
     public override void HandleLookAround(Vector2 dir, InputDevice device)
     {
-
+     
     }
 
 
-    public override void HandleAttack(bool held)
+    public override void HandleAttack(bool held) { }
+
+    protected override void HandleFlashlightSphereCast() { }
+
+    public override void HandleFlashlightPower() { }
+
+    public override void HandleRecharge()
     {
-
+        progress += ButtonMashBoost;
     }
-    
-    protected override void HandleFlashlightSphereCast()
-    {
-
-    }
-
-    public override void HandleFlashlightPower()
-    {
-
-    }
-
-
-
 }
-
