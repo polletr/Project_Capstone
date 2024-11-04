2f96033b2c23cf1699192ac8f9843078ce29e470
Musse <musse.hidru@outlook.com>
2024-11-03 23:00:15 -0700

    Reload crank

--- Assets/Scripts/Inventory/PlayerInventory.cs
+++ Assets/Scripts/Inventory/PlayerInventory.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     [field: SerializeField] public float ChargesCollected { get; private set; }
+    [field: SerializeField] public float CrankCollected { get; private set; }
 
-    private Stack<FlashlightAbility> collectedAbilitys = new();
+    private readonly Stack<FlashlightAbility> collectedAbility = new();
 
     private int numAbilityCollectedperCheckpoint;
     private float numBatteryCollectedperCheckpoint;
+    private float numCrankCollectedperCheckpoint;
 
     private List<int> _openDoorIDs = new();
 
     public GameEvent Event;
-
-
+    
     private void OnEnable()
     {
         Event.OnInteractItem += CollectItem;
         Event.OnTryToUnlockDoor += TryToOpenDoor;
         Event.OnLevelChange += RemoveAllKeys;
         Event.OnPlayerDeath += PlayerDiedRemoveAbility;
+        Event.OnPlayerDeath += RemoveBattery;
         Event.OnLevelChange += RestAbilityCheckpointCounter;
     }
 
@@ -30,6 +32,7 @@ public class PlayerInventory : MonoBehaviour
         Event.OnTryToUnlockDoor -= TryToOpenDoor;
         Event.OnLevelChange -= RemoveAllKeys;
         Event.OnPlayerDeath -= PlayerDiedRemoveAbility;
+        Event.OnPlayerDeath -= RemoveBattery;
         Event.OnLevelChange -= RestAbilityCheckpointCounter;
     }
 
@@ -46,7 +49,7 @@ public class PlayerInventory : MonoBehaviour
         }
     }
 
-    public void CollectItem(ICollectable item)
+    private void CollectItem(ICollectable item)
     {
         switch (item)
         {
@@ -55,19 +58,23 @@ public class PlayerInventory : MonoBehaviour
                 batteryItem.Collect();
                 break;
             case AbilityPickup abilityPickup:
-                collectedAbilitys.Push(abilityPickup.AbilityToPickup);
+                collectedAbility.Push(abilityPickup.AbilityToPickup);
                 numAbilityCollectedperCheckpoint++;
                 Event.OnPickupAbility?.Invoke(abilityPickup.AbilityToPickup);
                 abilityPickup.Collect();
                 break;
             case Key key:
-                if(!_openDoorIDs.Contains(key.OpenID))
-                _openDoorIDs.Add(key.OpenID);
+                if (!_openDoorIDs.Contains(key.OpenID))
+                    _openDoorIDs.Add(key.OpenID);
                 item.Collect();
                 break;
             case FlashlightPickup flashlightPickup:
                 Event.OnPickupFlashlight?.Invoke();
                 flashlightPickup.Collect();
+                break; 
+            case ReloadCrank crank:
+                AddCrank(crank.CrankBoost);
+                crank.Collect();
                 break;
             default:
                 Debug.Log($"Item not recognized wtf is this {item}");
@@ -77,39 +84,42 @@ public class PlayerInventory : MonoBehaviour
         // Display item in inventory
     }
 
-    public void AddBattery(float charge)
+    private void AddBattery(float charge)
     {
         AudioManagerFMOD.Instance.PlayOneShot(AudioManagerFMOD.Instance.SFXEvents.PickUpBatteries, transform.position);
         ChargesCollected += charge;
         numBatteryCollectedperCheckpoint += charge;
         Event.OnBatteryAdded?.Invoke(ChargesCollected);
     }
-
-    public void RemoveBattery()
+    private void AddCrank(float crank)
     {
-        ChargesCollected = ChargesCollected - numBatteryCollectedperCheckpoint;
+        AudioManagerFMOD.Instance.PlayOneShot(AudioManagerFMOD.Instance.SFXEvents.PickUpBatteries, transform.position);
+        CrankCollected += crank;
+        numCrankCollectedperCheckpoint += crank;
+        Event.OnCrankAdded?.Invoke(CrankCollected);
+    }
+
+    private void RemoveBattery()
+    {
+        ChargesCollected -= numBatteryCollectedperCheckpoint;
         Event.OnBatteryAdded?.Invoke(ChargesCollected);
     }
-    
 
-    public void RemoveAllKeys(LevelData data)
+
+    private void RemoveAllKeys(LevelData data)
     {
         _openDoorIDs.Clear();
     }
 
-    public bool HasKey(Door item)
-    {
-        return _openDoorIDs.Contains(item.OpenID);
-    }
-
     private void PlayerDiedRemoveAbility()
     {
-        while (numAbilityCollectedperCheckpoint > 0 && collectedAbilitys.Count > 0)
+        while (numAbilityCollectedperCheckpoint > 0 && collectedAbility.Count > 0)
         {
-            FlashlightAbility ability = collectedAbilitys.Pop();
+            var ability = collectedAbility.Pop();
             Event.OnRemoveAbility?.Invoke(ability);
             numAbilityCollectedperCheckpoint--;
         }
+
         RestAbilityCheckpointCounter(null);
     }
 
