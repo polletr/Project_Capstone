ed8ec7e0cad1f23902828c1ce0f1ba112ba6a501
Musse <musse.hidru@outlook.com>
2024-11-11 18:02:12 -0700

    Flashlight Pickup UI

--- Assets/Scripts/Interactables/FlashlightPickup.cs
+++ Assets/Scripts/Interactables/FlashlightPickup.cs
@@ -2,12 +2,12 @@ using UnityEngine.Events;
 
 public class FlashlightPickup : Interactable, ICollectable
 {
+    public PickupData FlashlightPickupData;
     public UnityEvent OnCollect;
     
     public void Collect()
     {
         AudioManagerFMOD.Instance.PlayOneShot(AudioManagerFMOD.Instance.SFXEvents.PickUpFlashlight, transform.position);
-
         gameObject.SetActive(false);//Destroy(this.gameObject);// uhhhhhhh disable it? pool this shit 
 
         OnCollect.Invoke();
@@ -17,5 +17,11 @@ public class FlashlightPickup : Interactable, ICollectable
     {
         base.OnInteract();
         Event.OnInteractItem?.Invoke(this);
+    }
+
+    public void ShowFlashlightPickupUI()
+    {
+        if (ObjectPickupUIHandler.Instance)
+            ObjectPickupUIHandler.Instance.PickedUpObject(FlashlightPickupData);
     }
 }
