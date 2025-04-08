5d9b697d5847003384db4cbe5dc283e0b552cdc6
Mos <musse.hidru@outlook.com>
2025-04-08 17:16:38 -0600

    Awesome Alarm

--- Assets/Scripts/AlarmClock.cs
+++ Assets/Scripts/AlarmClock.cs
@@ -1,5 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using FMOD.Studio;
+using Mono.Cecil;
+using UnityEngine.ProBuilder.MeshOperations;
+using UnityEngine.Serialization;
 
 public class AlarmClock : MonoBehaviour
 {
@@ -9,11 +16,19 @@ public class AlarmClock : MonoBehaviour
     [SerializeField] private float shakeDuration = 0.5f;
     [SerializeField] private float shakeStrength = 0.5f;
     [SerializeField] private Ease ease = Ease.Linear;
+    [SerializeField] private float dissolveDelay = 2f;
+    
+    [Header("References")]
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private DissolveEffectOnObject dissolve;
+    [SerializeField] private AudioHandler ringAudio;
+    [SerializeField] private AudioHandler dropAudio;
 
-    private Tween shakeTween;
-    private Tween ringTween;
+    private Tween ringingTween;
+    private Tween riseTween;
+    private Tween turnTween;
     private Sequence sequence;
-  
+
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
@@ -22,20 +37,42 @@ public class AlarmClock : MonoBehaviour
         
         sequence = DOTween.Sequence();
         sequence
-            .Append(ringTween = transform.DOMoveY(transform.position.y + jumpHeight, alarmMoveSpeed).SetEase(ease))
-            .Append(shakeTween = transform.DOShakeRotation(shakeDuration, shakeStrength).SetEase(ease));
+            .Append(riseTween = transform.DOMoveY(transform.position.y + jumpHeight, alarmMoveSpeed).SetEase(ease))
+            .Append(ringingTween = transform.DOShakeRotation(shakeDuration, shakeStrength, 10, 5f, false).SetEase(ease));
+            
         
-        shakeTween.OnComplete(() =>
+        riseTween.OnComplete(() =>
         {
-           //AudioManagerFMOD.Instance.PlayOneShot(AudioManagerFMOD.Instance.SFXEvents.RingingAlarm, transform.position);
-           triggerBox.enabled = true;
+            ringAudio.Play3DAudio(10f);
         });
+        
+        ringingTween.OnComplete(() =>
+        {
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            rb.AddTorque(Vector3.right, ForceMode.Impulse);
+            ringAudio.StopAudioInstance();
+        });
+     
         sequence.Play();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player")) return;
+        dropAudio.PlayOneShotAudio(transform);
+        StartCoroutine(DissolveDely());
+    }
+
+    private IEnumerator DissolveDely()
+    {
+        yield return new WaitForSeconds(dissolveDelay);
+        dissolve.DisableObj();
     }
 
     private void OnDestroy()
     {
-        ringTween?.Kill();
-        shakeTween?.Kill();
+        riseTween?.Kill();
+        ringingTween?.Kill();
     }
 }
