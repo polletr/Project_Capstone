d00bbb83c2171d5f25acb4d0a2aa16bd7b04333c
Renee Camillo Pollet <renee_p_35@hotmail.com>
2025-05-27 20:37:58 -0300

    More adjustments to scenes (Rollback)

--- Assets/Scripts/Levels/SceneNames.cs
+++ Assets/Scripts/Levels/SceneNames.cs
@@ -7,27 +7,9 @@ public enum SceneNames
     FirstWhiteRoom = 2,
     IntroReception = 3,
 
-    P1WhiteRoomSpawn = 4,
-    Reception = 5,
-    P2Dorm = 6,
-    P3MirrorPuzzle = 7,
-    
-    P4PaintingPuzzle = 8,
-    P5Infirmary = 9,
-    Lobotomy = 10,
-    ShockTherapy = 11,
-    
-    EndMenu = 12,
-    
-    TestMaster = 13,
-    LVL1 = 14,
-    LVL2 = 15,
-    LVL3 = 16,
-    LVL4 = 17,
-    LVL5 = 18,
-    LvL6 = 19,
-    
-    LVLDorm = 20
+    FirstReception = 4,
+    DormPuzzle = 5,
+    H2 = 6,
     
     
 }
